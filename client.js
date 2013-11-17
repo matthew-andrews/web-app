f152aaa46a7d533263b42b4074d296bb6b8e9d82
Matthew Andrews <matt@mattandre.ws>
2013-11-17 03:46:40 +0000

    Ever cloesr to render on server and client

--- client.js
+++ client.js
@@ -6,4 +6,8 @@ var model = require('./lib/models/article-client');
 
 app.base('/');
 app('/', index(model));
-app(/^\/([0-9]+)\/?$/, article(model));
+app(/^([0-9]+)\/?$/, article(model));
+
+// Dispatch with false because we should already have content,
+// unless we don't.
+app.start({ dispatch: false });
