245ad0a4268424773de0b77c17c44c1d21cc9e86
Matthew Andrews <matt@mattandre.ws>
2013-11-17 03:32:47 +0000

    Further work and experimentation between different ways to share JS.

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
 
+var model = require('./lib/models/article-server');
+
+var index = require('./lib/routes');
+var article = require('./lib/routes/article');
+var articleJson = require('./lib/routes/article-json');
+
 app.set('view engine', 'html');
 app.enable('view cache');
 app.engine('html', require('hogan-express'));
 
-app.get('/', require('./lib/routes'));
-app.get(/^\/([0-9]+)\/?$/, require('./lib/routes/article'));
+// Shared endpoints
+app.get('/', index(model));
+app.get(/^\/([0-9]+)\/?$/, article(model));
+
+// API only endpoints
+app.get('/api/articles.json', articleJson(model));
 
 app.use(express.static('public'));
 
