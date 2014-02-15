0f00263706fd3df6d76044de2949db0fd8300267
Matt Andrews <matt@mattandre.ws>
2014-02-15 02:33:00 +0000

    Remove more unwanted debug

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -6,7 +6,6 @@ var resources = require('../models/resource');
 module.exports = function(req, res) {
   Q.all([resources.get(), model.get(parseInt(req.params[0], 10))])
     .spread(function(resources, data) {
-    console.log("resources", resources);
       var view = fruitmachine({
         module: 'apple',
         model: data
