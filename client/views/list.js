0ae968b814c136beaeb1c8894bc39a7cb2f55141
Matt Andrews <matt@mattandre.ws>
2014-02-15 02:38:25 +0000

    Much cleaner way of doing client side fruit

--- client/views/list.js
+++ client/views/list.js
@@ -1,20 +1,13 @@
 var fruitmachine = require('fruitmachine');
 var template = require('../../views/partials/satsuma.html');
+var helpers = [require('fruitmachine-ftdomdelegate')];
 
-// HACK Don't allow delegate to be evaluated by node because it will complain HTML element is undefined
-if (typeof window !== 'undefined') {
-  var helpers = [require('fruitmachine-ftdomdelegate')];
-}
 
 module.exports = fruitmachine.define({
   name: 'satsuma',
   helpers: helpers,
   template: template,
   initialize: function() {
-
-    // TODO: Find a better way to do this
-    if (typeof window === 'undefined') return;
-
     this.on('initialize', function() {
       this.delegate.on('click', '#js-refresh', function() {
         this.fire('refreshbuttonclick');
