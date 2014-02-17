c0cdf0b57ad44ba73e792d5210150b8b5b37cd96
Matt Andrews <matt@mattandre.ws>
2014-02-17 00:36:09 +0000

    Clearer commenting

--- server/main.js
+++ server/main.js
@@ -15,18 +15,34 @@ app.set('view', require('./lib/view'));
 app.engine('html', require('hogan-express'));
 app.set('views', 'templates');
 
-// Api endpoints
+/**
+ * Api endpoints
+ */
+
 app.get('/api/articles.json', require('./controllers/api/article'));
 app.get(/^\/api\/article\/([0-9]+)\.json\/?$/, require('./controllers/api/article'));
 
-// Gui endpoints
+/**
+ * Gui endpoints
+ */
+
 app.get('/', resources, require('./controllers'));
 app.get(/^\/([0-9]+)\/?$/, resources, require('./controllers/article'));
 
-// Offline endpoints
+/**
+ * Offline endpoints
+ */
+
+// HACK: The iframe in which to attach an AppCache manifest to
 app.get('/offline/iframe', require('./controllers/offline/iframe'));
-app.get('/offline/manifest', require('./controllers/offline/manifest'));
+
+// HACK: Because we use fallbacks ajax request will now never fail - they will always be 200.
+// If we are truly offline we need to be able to figure out on the client side that the request
+// has failed.  So if the response is 'offline' we know we're offline :)
 app.get('/api/offline', require('./controllers/offline/api'));
+
+// The AppCache manifest itself
+app.get('/offline/manifest', require('./controllers/offline/manifest'));
 
 // Static endpoints
 app.use(express.static('public'));
